340fbe79336fc436ef87507a780ddcacc41b94f0
JoJoBond <JoJoBond@users.noreply.github.com>
2017-02-04 17:31:08 +0100

    Update livemp3.sh
    Update of git codebase from production system.

--- server/livemp3.sh
+++ server/livemp3.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ffmpeg -y -f alsa -i hw:0 -rtbufsize 64 -probesize 64 -acodec libmp3lame -ab 320k -reservoir 0 -ac 1 -f mp3 -write_xing 0 -id3v2_version 0 -flush_packets 1 -fflags +nobuffer -async 3 - | node stdinstreamer.js -port 9696 -type mp3 -burstsize 1
+ffmpeg -y -f alsa -ac 1 -i hw:0 -rtbufsize 64 -probesize 64 \
+-acodec libmp3lame -ab 320k -ac 1 -reservoir 0 -f mp3 \
+-fflags +nobuffer - \
+| nodejs stdinstreamer.js -port 9601 -type mp3 -burstsize 1
