68debf1e7390c283665afdcbfde35ac1cc6433f9
JoJoBond <JoJoBond@users.noreply.github.com>
2017-02-04 17:35:46 +0100

    Update livewave.sh

--- server/livewave.sh
+++ server/livewave.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-ffmpeg -y -ar 16000 -f alsa -i input1 -rtbufsize 384 -probesize 384 -acodec pcm_s16le -ac 1 -f wav -flush_packets 1 -fflags +nobuffer -chunk_size 384 -packetsize 384 -async 3 - | node stdinstreamer.js -port 9696 -type wav -chunksize 384
-
-#ffmpeg -y -ar 16000 -f alsa -i input1 -acodec pcm_s16le -ac 1 -f wav - | node stdinstreamer.js -port 9696 -type wav -chunksize 1024
+ffmpeg -y -f alsa -i hw:0 -rtbufsize 64 -probesize 64 \
+-acodec pcm_s16le -ar 16000 -ac 1 -f wav -fflags +nobuffer \
+-packetsize 384 - \
+| nodejs stdinstreamer.js -port 9602 -type wav -chunksize 384
