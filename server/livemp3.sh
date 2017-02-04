12becc1ad6f7d891d43676d703395f3423ee0886
JoJoBond <JoJoBond@users.noreply.github.com>
2017-02-04 17:34:24 +0100

    Update livemp3.sh

--- server/livemp3.sh
+++ server/livemp3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ffmpeg -y -f alsa -ac 1 -i hw:0 -rtbufsize 64 -probesize 64 \
+ffmpeg -y -f alsa -i hw:0 -rtbufsize 64 -probesize 64 \
 -acodec libmp3lame -ab 320k -ac 1 -reservoir 0 -f mp3 \
 -fflags +nobuffer - \
 | nodejs stdinstreamer.js -port 9601 -type mp3 -burstsize 1
